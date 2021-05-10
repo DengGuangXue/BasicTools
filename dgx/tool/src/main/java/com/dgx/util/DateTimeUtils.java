f0c7104c1ba6536a17c0ba2f2385964aeb55cf77
dengguangxue <1340798105@qq.com>
2021-05-10 23:46:12 +0800

    update project

---
+++ dgx/tool/src/main/java/com/dgx/util/DateTimeUtils.java
@@ -0,0 +1,15 @@
+package com.dgx.util;
+
+import java.util.Date;
+
+public class DateTimeUtils {
+    /**
+     *
+     * @param dateStr
+     * @param format
+     * @return
+     */
+    public static Date parse(String dateStr, String format) {
+        return new Date();
+    }
+}
